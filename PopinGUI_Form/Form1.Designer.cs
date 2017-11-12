4bc03f3779ec63679b1db27f445dd96413bb3031
Себастьян Перейро <m4r4d3r@hotmail.com>
2017-11-12 10:29:00 +0300

    Добавьте файлы проекта.

---
+++ PopinGUI_Form/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace PopinGUI_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ping";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(380, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "127.0.0.1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(289, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "О программе";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 125);
+            this.label1.MinimumSize = new System.Drawing.Size(380, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(380, 100);
+            this.label1.TabIndex = 3;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 251);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Popingui";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PopinGUI_Form/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace PopinGUI_Form
+{
+    public partial class Form1 : Form
+    {
+        Ping pingSender = new Ping();
+
+        public string address;
+
+        public int n;
+       
+        public async void DoPingThread()
+        {
+            try
+
+            {
+
+                PingReply reply;
+
+                while (n++ < 4)
+                {
+                    var result = await Task.Run(() =>
+                    {
+                    
+                        reply = pingSender.Send(address);
+                        Thread.Sleep(1000);
+                        return reply.Status;
+                    });
+
+                    if (result == IPStatus.Success)
+                    {
+
+                        label1.Text += "ICMP ответ получен - " + n + " Раз" + '\n';
+                        //System.Threading.Thread.Sleep(100);
+                    }
+                    else
+                    {
+                        label1.Text = "Ping'a нет!";
+                    }
+                }
+                //Task thread1 = new Task(PingFuncion);
+                //////Thread thread2 = new Thread(PingFuncion);
+
+
+                //////thread2.Start();
+                //int n = 0;
+                //while (n++ < 4)
+                //{
+                //    //PingFuncion();
+                //    thread1.Start();
+                //    thread1.Wait();
+
+                //}
+
+            }
+
+            catch (PingException)
+            {
+                label1.Text = "Некорректный ip адрес!";
+
+            }
+        }
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Получить ip адрес из textBox1
+            
+            address = textBox1.Text;
+
+            if (String.IsNullOrEmpty(address))
+            {
+                label1.Text = "Введите ip адрес";
+            }
+
+            else
+            {
+                // Пингуем хост
+                DoPingThread();
+                
+                
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            //label1.Text = textBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Hide();
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
